5dbbf5d1cdf9cd6c05ad1a67134414093c8e1da1
Harshit898 <60221014+Harshit898@users.noreply.github.com>
2020-01-23 19:26:39 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+ char str[20],str1[20];
+ int i,g;
+gets(str);
+gets(str1);
+for(i=0;i<20;i++)
+{
+ for(g=0;i<20;g++)
+ {
+  if(str1[g]!=str[i])
+  {
+   printf("no");
+  }
+  else
+  break;
+ }
+}
+}
